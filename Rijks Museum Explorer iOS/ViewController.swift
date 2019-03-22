819d15514da80c31e8712e649a715f5c72750f5f
Antonius George <atn010g@gmail.com>
2019-03-21 19:35:07 -0700

    Initial Commit

---
+++ Rijks Museum Explorer iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rijks Museum Explorer iOS
+//
+//  Created by user151601 on 3/21/19.
+//  Copyright © 2019 atn010.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
