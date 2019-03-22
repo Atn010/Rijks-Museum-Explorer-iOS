292b397e3fef0ea7acfb414571a6bc6c057096cd
Antonius George <atn010g@gmail.com>
2019-03-21 23:03:56 -0700

    Implemented basic application structure

---
+++ Rijks Museum Explorer iOS/View/HomeTVCell.swift
@@ -0,0 +1,27 @@
+//
+//  HomeTVCell.swift
+//  Rijks Museum Explorer iOS
+//
+//  Created by user151601 on 3/21/19.
+//  Copyright © 2019 atn010.com. All rights reserved.
+//
+
+import UIKit
+
+class HomeTVCell: UITableViewCell {
+
+	@IBOutlet weak var artImage: UIImageView!
+	@IBOutlet weak var artName: UILabel!
+	
+	override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
