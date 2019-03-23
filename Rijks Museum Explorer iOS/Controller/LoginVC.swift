7363b381808f145a3e00641cb2a588dd6425c5e5
Antonius George <atn010g@gmail.com>
2019-03-23 05:16:37 -0700

    Created Simple Login validation and verification

--- Rijks Museum Explorer iOS/Controller/LoginVC.swift
+++ Rijks Museum Explorer iOS/Controller/LoginVC.swift
@@ -10,11 +10,63 @@ import UIKit
 
 class LoginVC: UIViewController {
 
-    override func viewDidLoad() {
+	@IBOutlet weak var usernameTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
+	
+	
+	let userStatus = UserStatusSingleton.shared
+	override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+	
+	
+	@IBAction func loginClicked(_ sender: UIButton) {
+		
+		if attemptToLogin() {
+			performSegue(withIdentifier: "toMain", sender: self)
+		}
+		
+		
+		
+	}
+	
+	func showErrorMessage(message:String) {
+		let offlineAlert = UIAlertController(title: "Login Failed", message: message, preferredStyle: UIAlertController.Style.alert)
+		offlineAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+		self.present(offlineAlert,animated: true,completion: nil)
+	}
+	
+	func attemptToLogin() -> Bool {
+		
+		guard let username = usernameTextField.text else {
+			showErrorMessage(message: "Username should not be empty")
+			return false
+		}
+		
+		guard let password = passwordTextField.text else {
+			showErrorMessage(message: "Password should not be empty")
+			return false
+		}
+		
+		
+		if username.isEmpty {
+			showErrorMessage(message: "Username should not be empty")
+			return false
+		}else if password.isEmpty{
+			showErrorMessage(message: "Password should not be empty")
+			return false
+		}else if !userStatus.checkAccountValidity(username: username, password: password){
+			showErrorMessage(message: "Authentication failed")
+			return false
+		}
+		
+		
+		
+		return true
+	}
+	
     
 
     /*
